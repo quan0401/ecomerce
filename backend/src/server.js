baf8bd4fd2eab17ad0cf7a451db8f6d13162f3cf
quan0401 <dongminhquan2004@gmail.com>
2023-07-06 14:10:18 +0700

    before deploy 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -79,19 +79,19 @@ io.on("connection", (socket) => {
   });
 });
 
-// const path = require("path");
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../../frontend/build")));
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../../frontend", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.json({ message: "API running..." });
-//   });
-// }
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "API running..." });
+  });
+}
 
 // Use Helmet!
 app.use(helmet());
